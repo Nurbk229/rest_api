08d00b5569794eef27399ad92dddb4ff4834784e
OMEN <mrh61037@gmail.com>
2021-03-13 16:14:55 +0200

    update main

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:rest_api/services/note_service.dart';
 import 'package:rest_api/view/note_list.dart';
 
-void main() => runApp(App());
+void setUpLocator(){
+GetIt.I.registerLazySingleton(() => NoteServices());
+}
+void main() {
+  setUpLocator();
+  runApp(App());
+}
 
 class App extends StatelessWidget{
 
