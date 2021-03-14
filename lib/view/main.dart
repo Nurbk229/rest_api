59f4abb51fa70b67544507db7dd4652cd3a01b7f
OMEN <mrh61037@gmail.com>
2021-03-14 16:20:29 +0200

    update note modify

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -3,29 +3,23 @@ import 'package:get_it/get_it.dart';
 import 'package:rest_api/services/note_service.dart';
 import 'package:rest_api/view/note_list.dart';
 
-void setUpLocator(){
-GetIt.I.registerLazySingleton(() => NoteServices());
+void setUpLocator() {
+  GetIt.I.registerLazySingleton(() => NoteServices());
 }
+
 void main() {
   setUpLocator();
   runApp(App());
 }
 
-class App extends StatelessWidget{
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.blue
-      ),
+      theme: ThemeData(primaryColor: Colors.blue),
       home: NoteList(),
     );
-
   }
-
 }
-
-
-
